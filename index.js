27b05c0a72578113a23b8820f2371913eadfd713
Carlj0666 <carlj1881@yahoo.com>
2021-04-07 15:08:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+const test = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+const string1 = 'Bobby'
+const string2 = 'Sammy'
+
+
+function findMatching(array, string){
+    return array.filter(s => s.toLowerCase() === string.toLowerCase()
+  )
+}
+
+function fuzzyMatch(array, string) {
+  return array.filter(s => s.toLowerCase().indexOf(string.toLowerCase()) === 0
+  )
+}
+
+function matchName(array, string) {
+  return array.filter(s => s.name === string)
+}
